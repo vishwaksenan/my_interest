75c19939ac69887f012b6300d68bff5b4d382114
Vishwak Senan <43851281+vishwaksenan@users.noreply.github.com>
2018-10-04 22:28:11 +0530

    Update simple.py

--- simple.py
+++ simple.py
@@ -1,3 +1,5 @@
+#A simple neural network program for one input and output later. No hidden layer
+
 import numpy as np
 
 def  sigmoid(x,deriv=False):
@@ -8,17 +10,17 @@ def  sigmoid(x,deriv=False):
 
 
 input = np.array([[0,1,1],[1,0,0],[1,0,1],[1,1,1]])
-check = np.array([[0,1,1,1]]).T #4x1
+check = np.array([[0,1,1,1]]).T 
 
 np.random.seed(1)
 
 weight = 2*np.random.random((3,1))-1
 
 for i in range(1000000):
-    l0 = input #4x3
+    l0 = input 
     l1 = sigmoid(np.dot(l0,weight))
 
-    l1_error = check-l1 #4x1
+    l1_error = check-l1 
     l1_delta = l1_error * sigmoid(l1,True)
     weight = weight + np.dot(l0.T,l1_delta)
 
